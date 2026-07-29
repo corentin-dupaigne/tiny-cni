213255904da50836ce72e87235dd24ed08daff3f
corentin.dupaigne <declan.lowee@gmail.com>
2026-07-29 18:35:03 +0200

    feat(ipam): implement netip module for IPs arithmetic

--- internal/ipam/ipam.go
+++ internal/ipam/ipam.go
@@ -3,39 +3,50 @@ package ipam
 import (
 	"errors"
 	"fmt"
-	"math"
+	"net/netip"
 	"os"
-	"strconv"
-	"strings"
 )
 
 // hardcoded subnet (short term solution)
 const Subnet string = "172.17.17.0/24"
-const CounterPath string = "/tmp/tiny-cni-counter"
+const StoragePath string = "/tmp/tiny-cni-counter"
 
-func getNextIp(subnet string, counterPath string) (string, error) {
-	nextIp := ""
-	data, err := os.ReadFile(counterPath)
-	if errors.Is(err, os.ErrNotExist) {
-		_, after, _ := strings.Cut(subnet, "/")
-		prefixLength, _ := strconv.Atoi(after)
-		totalIps := math.Pow(2, 32-float64(prefixLength)) - 2
-		fileContent := []byte("0/" + strconv.Itoa(int(totalIps)))
-		err = os.WriteFile(counterPath, fileContent, 0644)
-		if err != nil {
-			return "", fmt.Errorf("Error while writing at path %s", counterPath)
-		}
-	} else if err != nil {
-		return "", fmt.Errorf("Error while reading file %s", counterPath)
+func getNextIp(subnet string, storagePath string) (string, error) {
+	var availableIp netip.Addr
+
+	prefix, err := netip.ParsePrefix(subnet)
+	if err != nil {
+		return "", fmt.Errorf("parsing prefix from subnet %s: %w", subnet, err)
 	}
 
-	currentIp, maxIp, _ := strings.Cut(string(data), "/")
-	if maxIp >= currentIp {
+	data, err := os.ReadFile(storagePath)
+	if errors.Is(err, os.ErrNotExist) {
+
+		networkIp := prefix.Masked().Addr()
+
+		availableIp = networkIp.Next().Next()
+
+	} else if err != nil {
+		return "", fmt.Errorf("reading file %s: %w", storagePath, err)
+	} else {
+		availableIp, err = netip.ParseAddr(string(data))
+		if err != nil {
+			return "", fmt.Errorf("parsing IP from file %s: %w", storagePath, err)
+		}
+	}
+
+	if !availableIp.IsValid() {
+		return "", fmt.Errorf("parsed IP is invalid: %s", availableIp)
+	}
+
+	if !prefix.Contains(availableIp.Next()) {
 		return "", fmt.Errorf("no IP addresses available in range %s", subnet)
 	}
-	lastPeriod := strings.LastIndexByte(subnet, '.')
-	before, _, _ := strings.Cut(subnet, string(subnet[lastPeriod]))
-	nextIp = before + "." + currentIp
 
-	return nextIp, nil
+	err = os.WriteFile(storagePath, []byte(availableIp.Next().String()), 0644)
+	if err != nil {
+		return "", fmt.Errorf("writing in file %s: %w", storagePath, err)
+	}
+
+	return availableIp.String(), nil
 }
